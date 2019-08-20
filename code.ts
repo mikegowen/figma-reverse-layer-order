a56c2177e6ec35803e8bc89fd88d2876717b35f9
Mike Gowen <mike@mikegowen.com>
2019-08-19 22:51:54 -0700

    Refactor to fix component unlinking bug

--- code.ts
+++ code.ts
@@ -1,63 +1,35 @@
 function main() {
   if (figma.currentPage.selection.length <= 1) {
-    figma.closePlugin("Please select 2 or more layers.");
-    return;
+    figma.closePlugin("Please select 2 or more layers.")
+    return
   }
 
   if (!haveSameParent(figma.currentPage.selection)) {
-    figma.closePlugin("Please select only layers in this same frame/group.");
-    return;
+    figma.closePlugin("Please select only layers in this same frame/group.")
+    return
   }
 
-  const selectionCopy = [...figma.currentPage.selection];
+  const selectedNodes = [...figma.currentPage.selection]
 
-  const sortedSelectionCopy = selectionCopy.sort((a, b) => {
-    return a.parent.children.indexOf(a) - b.parent.children.indexOf(b);
-  });
-
-  const selectionIndexes = selectionCopy.map(node => {
-    return node.parent.children.indexOf(node);
-  });
-
-  const sortedSelectionIndexes = selectionIndexes.sort((a, b) => {
-    return b - a;
-  });
-
-  const componentMap = {}
-  const clones = []
-
-  sortedSelectionIndexes.forEach((index, i) => {
-    let clone = sortedSelectionCopy[i].clone();
-    
-    if (clone.type === 'COMPONENT') {
-      componentMap[sortedSelectionCopy[i].id] = clone
-    }
-
-    sortedSelectionCopy[i].parent.insertChild(index, clone);
-    clone.x = sortedSelectionCopy[i].x;
-    clone.y = sortedSelectionCopy[i].y;
-    clones.push(clone)
-  });
-
-  clones.forEach(node => {
-    if (node.type === 'INSTANCE') {
-      if (Object.keys(componentMap).includes(node.masterComponent.id)) {
-        node.masterComponent = componentMap[node.masterComponent.id]
-      }
-    }
+  const selectedNodeParentIndexMap = selectedNodes.map(node => {
+    return { index: node.parent.children.indexOf(node), node: node }
   })
 
-  figma.currentPage.selection.forEach(node => {
-    node.remove();
-  });
+  selectedNodeParentIndexMap.sort((a, b) => a.index - b.index)
 
-  figma.closePlugin();
+  selectedNodeParentIndexMap.forEach((map, index) => {
+    let targetIndex = selectedNodeParentIndexMap[selectedNodeParentIndexMap.length - 1 - index].index
+    const currentIndex = map.node.parent.children.indexOf(map.node)
+    map.node.parent.insertChild(targetIndex, map.node)
+  })
+
+  figma.closePlugin()
 }
 
 function haveSameParent(nodes) {
   return nodes.every(node => {
-    return node.parent === nodes[0].parent;
-  });
+    return node.parent === nodes[0].parent
+  })
 }
 
-main();
+main()
